30c52168cf0bd862d13e250757a0debfd71aae0a
jwoon1013 <jwoon1013@gmail.com>
2023-06-01 10:58:11 +0900

    [1강] var let const

--- Week_1/01.js
+++ Week_1/01.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+// 실행 : 터미널에서 node 파일명
+
+// 변수, 상수
+// 변수 = 기억하고 싶은 값을 메모리에 저장한다. 읽어들여서 재사용한다.
+
+// 변수 이름 : 저장된 값의 고유 이름
+// 변수 값 : 변수에 저장된 값
+// 변수 할당 : 변수에 값을 저장하는 행위
+// 변수 선언 : 변수를 사용하기 위해 컴퓨터에 알리는 행위 (var myVar)
+// 변수 참조 : 변수에 할당된 값을 읽어오는 것
+
+// 변수 선언하는 3가지 방법 : var , let , const
+// 1. var
+var myVar = "Hello World";
+console.log(myVar);
+
+// 2. let
+let myLet = "Hello World2";
+console.log(myLet);
+
+// 3. const
+const myConst = "Hello World3"
+console.log(myConst);
